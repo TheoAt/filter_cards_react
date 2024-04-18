99e1a7bb8b75ab730274ed95d18fb6cf900d2511
TheoAt <theo.atlani@gmail.com>
2024-04-18 18:12:29 +0200

    fetching products and stylising

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import FilterCards from './components/FilterCards';
 function App() {
   return (
     <div className="main-container">
-      <h1 className="main-title">Filter Cards in ReactJS</h1>
+      <h1 className="main-title">Filter products by Category</h1>
       <FilterCards />
     </div>
   );
--- src/components/FilterCards.jsx
+++ src/components/FilterCards.jsx
@@ -1,9 +1,53 @@
+import { useEffect, useState } from 'react'
 import './filter.css'
 
 export default function FilterCards() {
-    return(
-        <div className="filter-container">
-            Test
-        </div>
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [products, setProducts] = useState([])
+
+    async function fetchProducts() {
+        try {
+            setIsLoading(true)
+            const apiResponse = await fetch('https://dummyjson.com/products', {
+                method: 'GET'
+            })
+
+            const result = await apiResponse.json()
+
+            if (result && result.products && result.products.length > 0) {
+                setIsLoading(false)
+                setProducts(result.products)
+            }
+
+        } catch (error) {
+            setIsLoading(false)
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    return (
+        <>
+            {isLoading ?
+                <p className="loading">Nous recherchons vos articles ! Merci de patientez quelques instants...</p>
+                :
+                <ul className="products-list">
+                    {products && products.length > 0 ?
+                        products.map(product => (
+                            <li key={product.id} className="product">
+                                <p className="product-name">{product.title}</p>
+                                <button className="product-category">{product.category}</button>
+                            </li>
+                        ))
+                        :
+                        null
+                    }
+                </ul>
+            }
+        </>
     )
 }
