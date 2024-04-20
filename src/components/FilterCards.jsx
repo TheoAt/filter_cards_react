36560463d8b04e65021b8f9af01ce2d3335fad07
TheoAt <theo.atlani@gmail.com>
2024-04-20 21:48:19 +0200

    add filter cards section and styles

--- src/components/FilterCards.jsx
+++ src/components/FilterCards.jsx
@@ -5,6 +5,8 @@ export default function FilterCards() {
 
     const [isLoading, setIsLoading] = useState(false)
     const [products, setProducts] = useState([])
+    const [filteredProducts, setFilteredProducts] = useState([])
+    const [currentSelectedCategory, setCurrentSelectedCategory] = useState('')
 
     async function fetchProducts() {
         try {
@@ -18,6 +20,7 @@ export default function FilterCards() {
             if (result && result.products && result.products.length > 0) {
                 setIsLoading(false)
                 setProducts(result.products)
+                setFilteredProducts(result.products)
             }
 
         } catch (error) {
@@ -30,23 +33,45 @@ export default function FilterCards() {
         fetchProducts()
     }, [])
 
+    useEffect(() => {
+        const cpyProducts = [...products]
+        setFilteredProducts(
+            currentSelectedCategory !== '' ?
+                cpyProducts.filter(product => product.category.toLowerCase() === currentSelectedCategory.toLowerCase())
+                :
+                cpyProducts
+        )
+
+    }, [currentSelectedCategory])
+
+    const categories = products && products.length > 0 ? [...new Set(products.map(product => product.category))] : []
+
     return (
         <>
             {isLoading ?
                 <p className="loading">Nous recherchons vos articles ! Merci de patientez quelques instants...</p>
                 :
-                <ul className="products-list">
-                    {products && products.length > 0 ?
-                        products.map(product => (
-                            <li key={product.id} className="product">
-                                <p className="product-name">{product.title}</p>
-                                <button className="product-category">{product.category}</button>
-                            </li>
-                        ))
-                        :
-                        null
-                    }
-                </ul>
+                <div className="filter-container">
+                    <div className="filter-categories-container">
+                        {categories.map(category => (
+                            <button className={`${currentSelectedCategory === category ? 'active' : ''}`} onClick={() => setCurrentSelectedCategory(currentSelectedCategory !== '' && currentSelectedCategory === category ? '' : category)}>
+                                {category}
+                            </button>
+                        ))}
+                    </div>
+                    <ul className="products-list">
+                        {filteredProducts && filteredProducts.length > 0 ?
+                            filteredProducts.map(product => (
+                                <li key={product.id} className="product">
+                                    <p className="product-name">{product.title}</p>
+                                    <button className="product-category">{product.category}</button>
+                                </li>
+                            ))
+                            :
+                            null
+                        }
+                    </ul>
+                </div>
             }
         </>
     )
